99d7e57dc9b1f63b05c2c1e49f1756cc24090952
Braulio Santos <brauliosantos12@outlook.com>
2024-03-08 19:26:24 +0000

    builtins all done

--- srcs/builtins/builtins.c
+++ srcs/builtins/builtins.c
@@ -37,13 +37,8 @@ int builtins(t_mini *mini)
 	if (!ft_strcmp(mini->args[0], "pwd"))
 		return (print_pwd());
 	if (!ft_strcmp(mini->args[0], "echo"))
-<<<<<<< HEAD
-		return (echo_cmd(mini));
-	if ((!ft_strcmp(mini->args[0], "cd")))
-=======
 		return (echo_cmd(mini->args, mini));
 	if ((!ft_strcmp(mini->args[0], "cd")) || (!ft_strcmp(mini->args[0], "~")))
->>>>>>> refs/remotes/origin/master
 		return (get_cd(mini));
 	if ((!ft_strcmp(mini->args[0], "env")))
 		return (get_envp(mini));
